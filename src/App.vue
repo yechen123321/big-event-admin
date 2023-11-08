f9895bb4cf1e545cf949265ce443be794603c091
chenye <3298365564@qq.com>
2023-11-08 21:51:28 +0800

    文章管理页面初设计

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
-  <!-- App.vue 只需要留一个路由出口 router-view -->
-  <router-view></router-view>
+  <!-- 国际化处理 -->
+  <el-config-provider :locale="zh">
+    <router-view />
+  </el-config-provider>
 </template>
-
 <style scoped></style>
 
-<script setup></script>
+<script setup>
+import zh from 'element-plus/es/locale/lang/zh-cn.mjs'
+</script>
