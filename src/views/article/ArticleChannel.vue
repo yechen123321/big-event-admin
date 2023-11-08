4d0fdfe5432e777849e926c1edb8d4689cbe136d
chenye <3298365564@qq.com>
2023-11-08 21:17:05 +0800

    文章分类功能实现

--- src/views/article/ArticleChannel.vue
+++ src/views/article/ArticleChannel.vue
@@ -1,8 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+import { Edit, Delete } from '@element-plus/icons-vue'
+import { artGetChannelsService, artDelChannelService } from '../../api/article'
+import ChannelEdit from './components/ChannelEdit.vue'
+const channelList = ref([])
+const loading = ref(false)
+const dialog = ref()
+
+const getChannelList = async () => {
+  loading.value = true
+  const res = await artGetChannelsService()
+  channelList.value = res.data.data
+  loading.value = false
+}
+getChannelList()
+
+const onDelChannel = async (row) => {
+  await ElMessageBox.confirm('你确认要删除该分类么', '温馨提示', {
+    type: 'warning',
+    confirmButtonText: '确认',
+    cancelButtonText: '取消'
+  })
+  await artDelChannelService(row.id)
+  ElMessage.success('删除成功')
+  getChannelList()
+}
+const onEditChannel = (row) => {
+  dialog.value.open(row)
+}
+const onAddChannel = () => {
+  dialog.value.open({})
+}
+const onSuccess = () => {
+  getChannelList()
+}
+</script>
+
 <template>
   <page-container title="文章分类">
     <template #extra>
       <el-button @click="onAddChannel">添加分类</el-button>
     </template>
+
     <el-table v-loading="loading" :data="channelList" style="width: 100%">
       <el-table-column type="index" label="序号" width="100"></el-table-column>
       <el-table-column prop="cate_name" label="分类名称"></el-table-column>
@@ -22,48 +61,18 @@
             circle
             plain
             type="danger"
-            @click="onDeleteChannel(row, $index)"
+            @click="onDelChannel(row, $index)"
           ></el-button>
         </template>
       </el-table-column>
+
       <template #empty>
         <el-empty description="没有数据"></el-empty>
       </template>
     </el-table>
-    <channel-Edit ref="dialog"></channel-Edit>
+
+    <channel-edit ref="dialog" @success="onSuccess"></channel-edit>
   </page-container>
 </template>
-
-<script setup>
-import { ref } from 'vue'
-import channelEdit from './components/ChannelEdit.vue'
-import { artGetChannelsService } from '@/api/article'
-import { Edit, Delete } from '@element-plus/icons-vue'
-const channelList = ref([])
-
-const loading = ref(false)
-
-const dialog = ref()
-
-const getChannelList = async () => {
-  loading.value = true
-  const res = await artGetChannelsService()
-  channelList.value = res.data.data
-  loading.value = false
-  console.log(channelList.value)
-}
-getChannelList()
-
-const onDeleteChannel = (row, $index) => {
-  console.log(row, $index)
-}
-
-const onEditChannel = (row) => {
-  dialog.value.open({ row })
-}
-const onAddChannel = () => {
-  dialog.value.open({})
-}
-</script>
 
 <style lang="scss" scoped></style>
